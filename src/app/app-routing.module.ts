aaf945910b74b139ea63f28beb2c0c07480ece21
gandruszkiewicz <gandruszkiewicz@gmail.com>
2020-09-24 23:25:48 +0200

    lockin init with enlink template

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+
+import { FullLayoutComponent } from "./layouts/full-layout/full-layout.component";
+import { CommonLayoutComponent } from "./layouts/common-layout/common-layout.component";
+
+import { FullLayout_ROUTES } from "./shared/routes/full-layout.routes";
+import { CommonLayout_ROUTES } from "./shared/routes/common-layout.routes";
+import { Login3Component } from './authentication/login-3/login-3.component';
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        redirectTo: '/dashboard/home',
+        pathMatch: 'full',
+    },
+    { 
+        path: '', 
+        component: CommonLayoutComponent,
+        children: CommonLayout_ROUTES 
+    },
+    { 
+        path: '', 
+        component: FullLayoutComponent, 
+        children: FullLayout_ROUTES
+    },
+    {
+        path:'login',
+        component: Login3Component
+    }
+    
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes, { 
+            preloadingStrategy: PreloadAllModules,
+            anchorScrolling: 'enabled',
+            scrollPositionRestoration: 'enabled' 
+        })
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule {
+}
