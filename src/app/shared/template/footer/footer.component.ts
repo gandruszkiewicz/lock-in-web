6e5d9fdabaaaa312668104ee0fb76435f4ea1469
gandruszkiewicz <gandruszkiewicz@gmail.com>
2020-10-02 22:09:30 +0200

    add logout btn ti side menu

--- src/app/shared/template/footer/footer.component.ts
+++ src/app/shared/template/footer/footer.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html'
 })
 
-export class FooterComponent{
+export class FooterComponent  implements OnInit{
+    FOOTER: any;
+    constructor(private translateService: TranslateService){}
+
+    ngOnInit(){
+        this.translateService.get("FOOTER").subscribe((translations) =>{
+            this.FOOTER = translations;
+          })
+    }
 }
--- src/app/shared/template/side-nav/side-nav.component.ts
+++ src/app/shared/template/side-nav/side-nav.component.ts
@@ -3,6 +3,7 @@ import { ROUTES } from './side-nav-routes.config';
 import { ThemeConstantService } from '../../services/theme-constant.service';
 import { TranslateService } from '@ngx-translate/core';
 import { SideNavInterface } from '../../interfaces/side-nav.type';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-sidenav',
@@ -15,17 +16,20 @@ export class SideNavComponent{
     isFolded : boolean;
     isSideNavDark : boolean;
     isExpand : boolean;
+    SIDE_MENU: any;
     my_information_menu_title: string;
 
     constructor( 
         private themeService: ThemeConstantService,
-        private translateService: TranslateService) {}
+        private translateService: TranslateService,
+        private router: Router) {}
 
     ngOnInit(): void {
         this.menuItems = ROUTES.filter(menuItem => menuItem);
-        this.translateService.get('SIDE_MENU.MY_INFORMATION')
-        .subscribe((text: string)=> {
-            this.my_information_menu_title = text;
+        this.translateService.get('SIDE_MENU')
+        .subscribe((translations)=> {
+            this.SIDE_MENU = translations
+            this.my_information_menu_title = this.SIDE_MENU.MY_INFORMATION;
             this.menuItems.forEach(x =>{
                 if(x.title === 'MY_INFORMATION'){
                     x.title = this.my_information_menu_title;
@@ -36,6 +40,12 @@ export class SideNavComponent{
         this.themeService.isMenuFoldedChanges.subscribe(isFolded => this.isFolded = isFolded);
         this.themeService.isExpandChanges.subscribe(isExpand => this.isExpand = isExpand);
         this.themeService.isSideNavDarkChanges.subscribe(isDark => this.isSideNavDark = isDark);
+    }
+
+    logoutClick(): void{
+        localStorage.clear();
+        this.router.navigate(['/login'])
+
     }
 
     closeMobileMenu(): void {
