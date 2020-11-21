37a8c4c9f43b2b8716f96feeda62e90e56716d6f
gandruszkiewicz <gandruszkiewicz@gmail.com>
2020-11-21 19:48:02 +0100

    error handlers

--- src/app/authentication/activation/activation-layout/activation-layout.component.ts
+++ src/app/authentication/activation/activation-layout/activation-layout.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AuthenticationService } from 'src/app/shared/services/authentication.service';
+import { MessagesService } from 'src/app/shared/services/messages/messages.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-activation-layout',
@@ -10,16 +12,27 @@ import { AuthenticationService } from 'src/app/shared/services/authentication.se
 export class ActivationLayoutComponent implements OnInit {
   isAuthenticated: boolean;
   userId: string;
+  ACCOUNT_ACTIVATION: any;
   constructor(
     private router: Router,
     private authService: AuthenticationService,
-    private activateRoute: ActivatedRoute) { }
+    private translateService: TranslateService,
+    private activateRoute: ActivatedRoute,
+    private messageService: MessagesService) { }
 
   ngOnInit(): void {
-    this.activateRoute.params.subscribe(params =>{
-      this.userId = params.userId;
-      this.authService.unlock(this.userId).subscribe((response)=>{
-        this.router.navigate(['login']);
+    this.translateService.get("ACCOUNT_ACTIVATION").subscribe((translations) =>{
+      this.ACCOUNT_ACTIVATION = translations;
+
+      this.activateRoute.params.subscribe(params =>{
+        this.userId = params.userId;
+        this.authService.unlock(this.userId).subscribe((response)=>{
+          this.messageService.message = {
+            isError: false,
+            content: this.ACCOUNT_ACTIVATION.SUCCESS
+          }
+          this.router.navigate(['login']);
+        })
       })
     })
   }
